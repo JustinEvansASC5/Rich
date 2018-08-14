09d896f2930a35a98507107f13ee1294bf5cfd3f
JustinE <20jevans@gmail.com>
2018-08-14 11:09:10 -0400

    first commit

---
+++ assets/js/js.js
@@ -0,0 +1,22 @@
+var money = 0;
+const LoginButton = document.getElementById("login")
+const AboutButton = document.getElementById("about")
+const ContactButton = document.getElementById("contact")
+const DonateButton = document.getElementById("donate")
+const body = document.getElementById("body")
+
+login.addEventListener("click", Loginpage)
+about.addEventListener("click", Aboutpage)
+contact.addEventListener("click", Contactpage)
+donate.addEventListener("click", Donatepage)
+
+//function Loginpage{}
+
+function Aboutpage(){
+}
+
+
+
+//function Contactpage{}
+
+//function Donatepage{}
