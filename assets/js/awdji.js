f99f26bfb0191a2a885858926d9624de9ff6a608
JustinE <20jevans@gmail.com>
2018-08-15 18:05:06 -0400

    lol

---
+++ assets/js/awdji.js
@@ -0,0 +1,20 @@
+const emailElement = document.querySelector("#email")
+const firstNameElement = document.querySelector("#firstName")
+const secondNameElement = document.querySelector("#secondName")
+const nameTitleElement = document.querySelector("#nameTitle")
+const monthlyIncomeElement = document.querySelector("#monthlyIncome")
+const musicElement= document.querySelector("#music")
+const shoppingElement= document.querySelector("#shopping")
+const eatingElement= document.querySelector("#eating")
+const allstarElement= document.querySelector("#allstarcode")
+const breastcancerElement= document.querySelector("breastcancer")
+const itemsbody = document.getElementById("importantitems")
+console.log(emailElement)
+
+function personalInfoEntered(){
+    const email = emailElement.value;
+    const firstName = firstNameElement.value;
+    const secondName = secondNameElement.value;
+    const nameTitle = nameTitleElement.value;
+    const monthlyIncome = monthlyIncomeElement.value;
+}
